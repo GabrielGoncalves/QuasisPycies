9e5fffb52e8328462c105acdc99ad31a1ddd86a1
GabrielGoncalves <gabrielgoncalvesbr@gmail.com>
2016-03-17 16:07:28 -0300

    Updated QuasisPycies.py file with python best practices indications and added polymorphic_sites function

--- QuasisPycies.py
+++ QuasisPycies.py
@@ -2,9 +2,29 @@
 # Author: Gabriel dos Santos Goncalves
 # Rio de Janeiro, Brasil - march 2016
 
+'''
 
-# QuasiPycies
+       =======  QuasiPycies =======
 
+Module to calculate diversity indices from NGS data. It consists of two main 
+classes (Clusters and Pileup) and extra functions for convertion and parsing 
+of different file formats.
+
+class Clusters 
+
+	
+
+	Functions : shannon_entropy
+				simpsons_index
+				simpsons_index_of_diversity
+				simpsons_Reciprocal_Index
+				clusters_object_to_fasta
+				filtering_clusters_by_percentage
+				number_haplotypes
+				filtering_Clusters
+
+
+'''
 import re 
 import math
 
@@ -52,11 +72,10 @@ class Clusters(object):
       
     
     def shannon_entropy(self, minimun_perc):
-    	''' Shannon Entropy is a diversity index that measures the evenness of
-    	the population. It uses the values of size of haplotypes 
-    	(subpopulations) in the sample.  
-    	'''
-        
+        ''' Shannon Entropy is a diversity index that measures the evenness of
+        the population. It uses the values of size of haplotypes 
+        (subpopulations) in the sample.  
+        '''
         number_reads = 0
         list_sizes = []
 
@@ -65,10 +84,8 @@ class Clusters(object):
             if value[1] >=  minimun_perc:
                 list_sizes.append(value[2])
                 number_reads += value[2]
-        
-        #print(list_sizes)
-        
         entropy = 0
+
         for v in list_sizes:
             subpop = (v/number_reads) * math.log(v/number_reads)
             entropy += subpop
@@ -79,51 +96,59 @@ class Clusters(object):
 
     # 
     def simpsons_index(self):
-    	''' Simpson's index measures the probability that two individuals 
-    	(in this case reads) randomly selected from a sample will belong 
-    	to the same species (in this case cluster).
-    	'''
-    	
+        ''' Simpson's index measures the probability that two individuals 
+        (in this case reads) randomly selected from a sample will belong 
+        to the same species (in this case cluster).
+        '''
+        
         D = 0
-	    for element in self.number_reads:
-	        d= (element/sum(self.number_reads))**2
-	        D += d
-	        
-	    return(D)
+        for element in self.number_reads:
+            d= (element/sum(self.number_reads))**2
+            D += d
+            
+        return(D)
 
-	def simpsons_index_of_diversity(self):
-		''' Simpson's index of diversity measures the probability that two
-		individuals (in this case reads) randomly selected from a sample will
-		belong to different species (in this case cluster). 
+    def simpsons_index_of_diversity(self):
+        ''' Simpson's index of diversity measures the probability that two
+        individuals (in this case reads) randomly selected from a sample will
+        belong to different species (in this case cluster). 
 
-		Simpson's index of diversity = 1 - Simpson's index
-		'''
+        Simpson's index of diversity = 1 - Simpson's index
+        '''
 
-	    D = 0
-	    for element in self.number_reads:
-	        d= (element/sum(self.number_reads))**2
-	        D += d
-	        
-	    return(1-D)
+        D = 0
+        for element in self.number_reads:
+            d= (element/sum(self.number_reads))**2
+            D += d
+            
+        return(1-D)
 
 
-	def simpsons_Reciprocal_Index(self):
-    ''' Simpson's Reciprocal Index 
+    def simpsons_reciprocal_index(self):
+        ''' Simpson's Reciprocal Index 
 
-    '''
+        Simpson's Reciprocal Index = 1 / Simpson's index
+        '''
+        D = 0
+        for element in self.number_reads:
+            d= (element/sum(self.number_reads))**2
+            D += d
+            
+        return(1/D)
 
-	    D = 0
-	    for element in self.number_reads:
-	        d= (element/sum(self.number_reads))**2
-	        D += d
-	        
-	    return(1/D)
+
+        D = 0
+        for element in self.number_reads:
+            d= (element/sum(self.number_reads))**2
+            D += d
+            
+        return(1/D)
 
 
     # This function writes to a fasta file the information of self.dict_fasta
     def clusters_object_to_fasta(self, fasta_name):
-    	''' This function creates a fasta file from the dict_fasta object
-    	'''
+        ''' This function creates a fasta file from the dict_fasta object
+        '''
         
         # First we create a file to receive the data
         fasta_output = open(fasta_name, "w")
@@ -135,8 +160,8 @@ class Clusters(object):
 
     
     def filtering_clusters_by_percentage(self, minimun_perc, output1):
-    	''' This function filters the clusters by percentage.
-    	'''
+        ''' This function filters the clusters by percentage.
+        '''
 
         #file_in = open (input1, "r")
         #fasta_in = file_in.readlines()
@@ -161,11 +186,13 @@ class Clusters(object):
                 #file_out.write(">" + name + "_" + value[2]+ "_" + str(round(float((value[2]/number_reads)*100),2)) + "%" + "\n")
                 file_out.write(value[0])
                 
-    def number_haplotypes(self, value_type, cutoff):
-    	''' This function counts the number of haplotypes greater than a 
-    	cutoff. The cutoff can be "percentage" or "number_reads".
 
-    	'''
+    def number_haplotypes(self, value_type, cutoff):
+        ''' This function counts the number of haplotypes greater than a 
+        cutoff. The cutoff can be "percentage" or "number_reads" (must discribe
+        in value_type parameter).
+
+        '''
 
         # Define the two variables to hold the number of haplotypes
         counter_haplotypes = 0
@@ -191,7 +218,7 @@ class Clusters(object):
     # This function filter the clusters base the percentage or number of reads cutoof
     # and returns the dict_fasta updated
     
-    def filtering_Clusters(self, value_type, cutoff):
+    def filtering_clusters(self, value_type, cutoff):
 
             self.filtered_dict_fasta = {}
 
@@ -215,12 +242,10 @@ class Clusters(object):
 
 
 
-
-
-'''		
-# This class is in charge of dealing with pileup files, parsing it and calculating
-# diversity indices like nucleotide diversity
-class Pileup(object):
+class Pileup(object):     
+    '''This class is in charge of dealing with pileup
+    files, parsing it and calculating # diversity indices like nucleotide diversity
+    '''     
 
     def __init__(self, pileup_file, min_reads_per_position=0):
         
@@ -248,78 +273,6 @@ class Pileup(object):
                 pos_bases_sub = re.sub("[.,]",ref_base, pos_bases_original).upper()
                 self.dict_pileup[line_number]= pos_bases_sub
 
-
-    # This function calculates the nucleotide diversity of the Pileup object
-	def nucleotide_diversity(self):
-		# First we create a list to hold the nucleotide diversity of each base position
-        nuc_diversity_list=[]
-
-        # Iterate over each value in the dict_pileup (representing the polymorphisms of each position)
-        for pos in self.dict_pileup.values():
-                
-            # Count the number of each bases in the string
-            A = pos.upper().count("A")
-            T = pos.upper().count("T")
-            C = pos.upper().count("C")
-            G = pos.upper().count("G")
-            reads = A + C + T + G
-            Di = (float((A*C)+(A*G)+(A*T)+(C*G)+(C*T)+(G*T)))/(float((reads**2 - reads)/2))
-                
-            # Store the value nucleotide diversity in this position to the list
-            nuc_diversity_list.append(Di)
-
-        # Calculate average nucleotide diversity buy adding all the values in the list
-        # and dividing by the number of base positions (number of itens in the dict_pileup)
-        avg_nuc_diversity = sum(nuc_diversity_list)/len(self.dict_pileup.values())
-        return(round(avg_nuc_diversity,4))
-
-    # This function counts the number of polymorphic sites 
-    def polymorphic_sites(self, value_type, cutoff):
-    	
-    	number_polymorphic_sites = 0
-
-    	for key, value in dict_pileup.items():
-    		A = value.upper().count("A")
-        	T = value.upper().count("T")
-        	C = value.upper().count("C")
-        	G = value.upper().count("G")
-
-        	list_n_bases = [A,T,C,G]
-        	list_n_bases.sort
-
-        	if list_n_bases[2] > cutoff:    
-                    number_polymorphic_sites += 1
-
-        return(number_polymorphic_sites)
-'''
-
-class Pileup(object):     '''This class is in charge of dealing with pileup
-files, parsing it and calculating # diversity indices like nucleotide diversity
-'''     def __init__(self, pileup_file, min_reads_per_position=0):
-        
-        # Create the dictionary to store each base data
-        self.dict_pileup = {}
-
-        #Load the file and read lines
-        self.pileup_file = open(pileup_file, "r").readlines()
-        
-        # Iterate over the lines of the pileup file storing the information 
-        # of each base position  
-        for n,line in enumerate(self.pileup_file):
-            line_elements =  line.split("\t")
-            ref= line_elements[0]
-            line_number = line_elements[1]
-            ref_base = line_elements[2]
-            pos_depth= line_elements[3]
-            base_qualities= line_elements[5]
-            pos_bases_original = line_elements[4]
-            if n < min_reads_per_position:
-                pos_bases_sub_1 = re.sub(r"\^.([ACTGNacgtn.,])", r"\1", pos_bases_original).upper()    
-                pos_bases_sub_2 = re.sub("[.,]",ref_base, pos_bases_sub_1).upper()
-                self.dict_pileup[line_number]= pos_bases_sub_2
-            else:
-                pos_bases_sub = re.sub("[.,]",ref_base, pos_bases_original).upper()
-                self.dict_pileup[line_number]= pos_bases_sub
     
     def nucleotide_diversity(self):
             # dict_pileup = parsing_pileup(input_mpileup)
@@ -340,158 +293,154 @@ files, parsing it and calculating # diversity indices like nucleotide diversity
             #print(round(avg_nuc_diversity,4))
             return(round(avg_nuc_diversity,4))
 
+
     def polymorphic_sites(self, value_type, cutoff):
 
             number_polymorphic_sites = 0
 
             if value_type == "percentage":
-            	for key, value in self.dict_pileup.items():
-	                A = value.upper().count("A")
-	                T = value.upper().count("T")
-	                C = value.upper().count("C")
-	                G = value.upper().count("G")
+                for key, value in self.dict_pileup.items():
+                    A = value.upper().count("A")
+                    T = value.upper().count("T")
+                    C = value.upper().count("C")
+                    G = value.upper().count("G")
 
-	                list_n_bases = [A,T,C,G]
-	                list_n_bases.sort
+                    list_n_bases = [A,T,C,G]
+                    list_n_bases.sort()
+                    #print(list_n_bases)
+                    
+                    if list_n_bases[2] > sum(list_n_bases) * cutoff:    
+                        number_polymorphic_sites += 1
 
-	                #if list_n_bases[2] < sum(list_n_bases)*(cutoff/100):
-	                if list_n_bases[2] > cutoff:    
-	                    number_polymorphic_sites += 1
+            if value_type == "number_reads":
+                for key, value in self.dict_pileup.items():
+                    A = value.upper().count("A")
+                    T = value.upper().count("T")
+                    C = value.upper().count("C")
+                    G = value.upper().count("G")
 
-	        if value_type == "number_reads":
-            	for key, value in self.dict_pileup.items():
-	                A = value.upper().count("A")
-	                T = value.upper().count("T")
-	                C = value.upper().count("C")
-	                G = value.upper().count("G")
+                    list_n_bases = [A,T,C,G]
+                    list_n_bases.sort()
 
-	                list_n_bases = [A,T,C,G]
-	                list_n_bases.sort
-
-	                #if list_n_bases[2] < sum(list_n_bases)*(cutoff/100):
-	                if list_n_bases[2] > cutoff:    
-	                    number_polymorphic_sites += 1
-
-
+                    #if list_n_bases[2] < sum(list_n_bases)*(cutoff/100):
+                    if list_n_bases[2] > cutoff:
+                            
+                        number_polymorphic_sites += 1
 
 
             return(number_polymorphic_sites)
 
 
 
-
-
-
-
 # These are python functions that may be used on the main workflow
 def filtering_clusters_by_percentage(input1, minimun_perc, output1):
 
-	file_in = open (input1, "r")
-	fasta_in = file_in.readlines()
-	file_out = open (output1, "w")
-	number_reads = 0
-	list_sizes = []
+    file_in = open (input1, "r")
+    fasta_in = file_in.readlines()
+    file_out = open (output1, "w")
+    number_reads = 0
+    list_sizes = []
 
-	for number, line in enumerate(fasta_in):
-		if line[0]==">":
-			n_search = re.search(";size=(\d+)", line)
-			n = int(n_search.group(1))
-			number_reads += n
-
-
-	for number, line in enumerate(fasta_in):
-		if line[0]==">":
-			n_search = re.search(";size=(\d+)", line)
-			n = int(n_search.group(1))
-			if n >= (number_reads * float(minimun_perc)):
-				file_out.write(line.rstrip() + str(round(float((n/number_reads)*100),2)) + "%" + "\n")
-				file_out.write(fasta_in[number+1])
+    for number, line in enumerate(fasta_in):
+        if line[0]==">":
+            n_search = re.search(";size=(\d+)", line)
+            n = int(n_search.group(1))
+            number_reads += n
 
 
-def SAM_to_fasta(SAM_file, fasta_file):
+    for number, line in enumerate(fasta_in):
+        if line[0]==">":
+            n_search = re.search(";size=(\d+)", line)
+            n = int(n_search.group(1))
+            if n >= (number_reads * float(minimun_perc)):
+                file_out.write(line.rstrip() + str(round(float((n/number_reads)*100),2)) + "%" + "\n")
+                file_out.write(fasta_in[number+1])
 
-	SAM = open (SAM_file, "r")
-	SAM_lines = SAM.readlines()
-	fasta_file = open (fasta_file, "w")
 
-	for line in SAM_lines:
-		if line[0] != "@":
-			line_elements = line.split("\t")
-			fasta_file.write(">" + str(line_elements[0] + "\n"))
-			fasta_file.write(line_elements[9]+ "\n")
+def sam_to_fasta(SAM_file, fasta_file):
+
+    SAM = open (SAM_file, "r")
+    SAM_lines = SAM.readlines()
+    fasta_file = open (fasta_file, "w")
+
+    for line in SAM_lines:
+        if line[0] != "@":
+            line_elements = line.split("\t")
+            fasta_file.write(">" + str(line_elements[0] + "\n"))
+            fasta_file.write(line_elements[9]+ "\n")
 
 
 
 # This function converts fastq files into fasta files
 def fastq_to_fasta(fastq_file, fasta_file):
-	startTime = datetime.now()
+    startTime = datetime.now()
 
-	# Open input fastq file and create output fasta file 
-	fastq1 = open (fastq_file, "r")
-	fastq = fastq1.readlines()
-	fasta = open (fasta_file, "w")
-	
-	print("Number of lines in the file " + str(len(fastq)))
+    # Open input fastq file and create output fasta file 
+    fastq1 = open (fastq_file, "r")
+    fastq = fastq1.readlines()
+    fasta = open (fasta_file, "w")
+    
+    print("Number of lines in the file " + str(len(fastq)))
 
-	# Create lists with the number of the lines carrying the name of reads and bases 
-	list_name_lines = [] # 0
-	list_bases_lines = [] # 1
-	 
-	for a in range(0,(len(fastq)+1),4):
-		list_name_lines.append(a)
-	
-	for b in range(1,(len(fastq)+1),4):
-		list_bases_lines.append(b)
-	
-	# Iterate through each line in the file and copy only the ones that represent the
-	# name and the bases of each read to the new fasta file. Also replacing the "@" for 
-	# the ">" before each read name 	
-	for number, line in enumerate(fastq):
-		if number in list_name_lines:
-			mod1 = re.sub('^@','>', line)
-			fasta.write(mod1)
-			
-		elif number in list_bases_lines:
-			fasta.write(line)
-	# Print the time it took to convert fastq to fasta
-	print ("Time to convert fastq to fasta: ",datetime.now() - startTime)
+    # Create lists with the number of the lines carrying the name of reads and bases 
+    list_name_lines = [] # 0
+    list_bases_lines = [] # 1
+     
+    for a in range(0,(len(fastq)+1),4):
+        list_name_lines.append(a)
+    
+    for b in range(1,(len(fastq)+1),4):
+        list_bases_lines.append(b)
+    
+    # Iterate through each line in the file and copy only the ones that represent the
+    # name and the bases of each read to the new fasta file. Also replacing the "@" for 
+    # the ">" before each read name     
+    for number, line in enumerate(fastq):
+        if number in list_name_lines:
+            mod1 = re.sub('^@','>', line)
+            fasta.write(mod1)
+            
+        elif number in list_bases_lines:
+            fasta.write(line)
+    # Print the time it took to convert fastq to fasta
+    print ("Time to convert fastq to fasta: ",datetime.now() - startTime)
 
 
 # This function converts multiline fasta into single line
-def MultiLine_fasta_to_SingleLine(input1, output1):
+def multiline_fasta_to_singleline(input1, output1):
 
-	fasta = open (input1, "r")
-	fasta_ML = fasta.readlines()
-	fasta_SL = open (output1, "w")
+    fasta = open (input1, "r")
+    fasta_ML = fasta.readlines()
+    fasta_SL = open (output1, "w")
 
-	for n,line in enumerate(fasta_ML):
-		if line.startswith(">"):
-			fasta_ML[n] =  "\n" + line.rstrip() 
-		else:
-			fasta_ML[n]=line.rstrip()
+    for n,line in enumerate(fasta_ML):
+        if line.startswith(">"):
+            fasta_ML[n] =  "\n" + line.rstrip() 
+        else:
+            fasta_ML[n]=line.rstrip()
 
-	data = ('|'.join(fasta_ML))
+    data = ('|'.join(fasta_ML))
 
-	data = data[1:]
-	
-	x = re.sub(";\|","\n",data)
-	y = re.sub("\|","",x)
-	fasta_SL.write(y+"\n")
+    data = data[1:]
+    
+    x = re.sub(";\|","\n",data)
+    y = re.sub("\|","",x)
+    fasta_SL.write(y+"\n")
 
 # This function transform clusters back to single reads
 def clusters_to_single_reads(input1, output1):
 
-	fasta = open (input1, "r")
-	fasta_ML = fasta.readlines()
-	fasta = open (output1, "w")
+    fasta = open (input1, "r")
+    fasta_ML = fasta.readlines()
+    fasta = open (output1, "w")
 
-	for n,line in enumerate(fasta_ML):
-		if line.startswith(">"):
-			count_match = re.search("size=(\d+)", line)
-			count_number = int(count_match.group(1))
-			read_name = line[:line.find(";size")]
-			
-			for i in range(0,count_number):
-				fasta.write(read_name + "_" + str(i)+"\n")
-				fasta.write(fasta_ML[n+1])
+    for n,line in enumerate(fasta_ML):
+        if line.startswith(">"):
+            count_match = re.search("size=(\d+)", line)
+            count_number = int(count_match.group(1))
+            read_name = line[:line.find(";size")]
+            
+            for i in range(0,count_number):
+                fasta.write(read_name + "_" + str(i)+"\n")
+                fasta.write(fasta_ML[n+1])
 
